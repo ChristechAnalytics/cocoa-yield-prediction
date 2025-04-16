815b151bdccbf81659f9b3a8629eb3cad2b322f1
Christopher Onyeneke <conyeneke@gmail.com>
2025-04-16 08:30:41 +0100

    perform feature engineering and use key feature for prediction

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -8,17 +8,16 @@ model = joblib.load('src/cocoa_model.pkl')
 scaler = joblib.load('src/scaler.pkl')
 
 st.sidebar.header("Enter Climate Data")
-rainfall = st.sidebar.slider("Rainfall (mm)", 2200, 2900, 2500)
-min_temp = st.sidebar.slider("Min Temperature (°C)", 23.0, 25.0, 23.8)
-max_temp = st.sidebar.slider("Max Temperature (°C)", 30.0, 32.0, 30.9)
 solar = st.sidebar.slider("Solar Radiation (kw/m2)", 1.8e6, 2.1e6, 2e6)
-wind = st.sidebar.slider("Wind Speed (m/s)", 38.0, 45.0, 40.0)
-min_rh = st.sidebar.slider("Min RH (%)", 55, 65, 60)
-max_rh = st.sidebar.slider("Max RH (%)", 90, 96, 93)
+min_temp = st.sidebar.slider("Min Temperature (°C)", 23.0, 25.0, 23.8)
+# max_temp = st.sidebar.slider("Max Temperature (°C)", 30.0, 32.0, 30.9)
+# rainfall = st.sidebar.slider("Rainfall (mm)", 2200, 2900, 2500)
+# wind = st.sidebar.slider("Wind Speed (m/s)", 38.0, 45.0, 40.0)
+# min_rh = st.sidebar.slider("Min RH (%)", 55, 65, 60)
+# max_rh = st.sidebar.slider("Max RH (%)", 90, 96, 93)
 
-input_df = pd.DataFrame([[rainfall, min_temp, max_temp, solar, wind, min_rh, max_rh]],
-                         columns=["Rainfall_mm", "Min_Temp_C", "Max_Temp_C", "Solar_Radiation_kwm2",
-                                  "Wind_Speed_ms", "Min_RH", "Max_RH"])
+input_df = pd.DataFrame([[min_temp, solar]],
+                         columns=["Min_Temp_C", "Solar_Radiation_kwm2"])
 input_scaled = scaler.transform(input_df)
 
 if st.button("Predict Cocoa Yield"):
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,7 +6,8 @@ def load_data(filepath):
 
 def preprocess_data(df):
     features = df.drop(columns=['Year', 'Cocoa_Yield'])
+    reduced_features = features.drop(columns=['Min_RH', 'Max_RH', 'Rainfall_mm', 'Wind_Speed_ms', 'Max_Temp_C'])
     target = df['Cocoa_Yield']
     scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(features)
+    scaled_features = scaler.fit_transform(reduced_features)
     return scaled_features, target, scaler
